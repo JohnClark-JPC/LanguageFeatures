684d5239bbdf646e565772cee4acfb1b51e82611
John Clark <john.clark012@outlook.com>
2020-08-22 00:55:17 -0400

    Complete through pg 92.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+        bool FilterByPrice(Product p)
+        {
+            return (p?.Price ?? 0) >= 20;
+        }
         public ViewResult Index()
         {
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
@@ -18,9 +22,13 @@ namespace LanguageFeatures.Controllers
                                        new Product { Name = "Soccer ball", Price = 19.50M},
                                        new Product { Name = "Corner flag", Price = 34.95M}
                                      };
+            Func<Product, bool> nameFilter = delegate (Product prod)
+            {
+                return prod?.Name?[0] == 'S';
+            };
 
-            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
-            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20).TotalPrices();
+            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0] == 'S').TotalPrices();
 
             return View("Index", new string[] { $"Price Total: {priceFilterTotal:C2}",
                                                 $"Name Total: {nameFilterTotal:C2}" });
