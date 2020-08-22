6dbd289bcfd7797bac1755201c74470636e31880
John Clark <john.clark012@outlook.com>
2020-08-22 00:45:38 -0400

    Complete through pg 90

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,18 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View("Index", new string[] { "Bob", "Joe", "Alice" });
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            Product[] productArray = { new Product { Name = "Kayak", Price = 275M},
+                                       new Product { Name = "Lifejacket", Price = 48.95M},
+                                       new Product { Name = "Soccer ball", Price = 19.50M},
+                                       new Product { Name = "Corner flag", Price = 34.95M}
+                                     };
+
+            decimal priceFilterTotal = productArray.FilterByPrice(20).TotalPrices();
+            decimal nameFilterTotal = productArray.FilterByName('S').TotalPrices();
+
+            return View("Index", new string[] { $"Price Total: {priceFilterTotal:C2}",
+                                                $"Name Total: {nameFilterTotal:C2}" });
         }
     }
 }
